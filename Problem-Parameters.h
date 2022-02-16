0ab340688fc2367b16a8ef223f697e24b22c422a
Nikolay Olkhovskiy <nikolay.olkhovsky@gmail.com>
2022-02-16 11:48:09 +0500

    Save changes from supercomputer.

--- Problem-Parameters.h
+++ Problem-Parameters.h
@@ -12,11 +12,11 @@ This source code is developed based on the BSF skeleton (https://github.com/leon
 #define PP_N		3									// Number of dimensions
 #define PP_MAX_M	250									// Maximal  number of inequalities
 #define PP_MAX_N	120									// Maximal  space dimension
-#define PP_ETA		5									// Rank of receptive field 		
-#define PP_DELTA	1									// Density of receptive field
+#define PP_ETA		2									// Rank of receptive field 		
+#define PP_DELTA	2									// Density of receptive field
 #define PP_MAX_K	18446744073709550592ull				// Maximal number of receptive field points
-#define PP_PATH		"C:/Users/Akella/source/repos/nikolay-olkhovskiy/LP-visualization-MPI/"	// Working directory of the application
-//#define PP_PATH		"/home/olkhovskiina/"	// Working directory of the application
+//#define PP_PATH		"C:/Users/Akella/source/repos/nikolay-olkhovskiy/LP-visualization-MPI/"	// Working directory of the application
+#define PP_PATH		"/home/olkhovskiina/"	// Working directory of the application
 #define PP_LPP_FILE	"model_example.txt"	// File with initial data
 #define PP_OUT_FILE "image.txt"			// File with output results
 #define PP_INPUT_FILE	"point.txt"		// File with coordinates of target point
